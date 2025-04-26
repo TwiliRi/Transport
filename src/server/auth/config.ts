0b8aeda8d23efacd55b14aeeb230e5e32d2f6f72
TwiliRi <rinatlausin449@gmail.com>
2025-04-26 12:10:40 +0300

    micro update

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,6 +1,5 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
 import VkProvider from "next-auth/providers/vk";
 import jwt from "jsonwebtoken";
@@ -35,7 +34,6 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
