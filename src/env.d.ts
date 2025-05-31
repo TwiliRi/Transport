871e70e983d0bcb67e0bbf492f61c66ca5b877dd
TwiliRi <rinatlausin449@gmail.com>
2025-06-01 01:16:13 +0300

    Решена проблема .env теперь по нормальному

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,16 @@
+declare module '~/env' {
+  export const env: {
+    NEXTAUTH_SECRET: string;
+    AUTH_SECRET: string;
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+    DATABASE_URL: string;
+    NODE_ENV: 'development' | 'test' | 'production';
+  };
+}
+
 declare namespace NodeJS {
+  
   interface ProcessEnv {
     NEXTAUTH_SECRET: string;
     AUTH_SECRET: string;
